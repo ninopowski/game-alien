afbe4584834193841ce90da8ad32166ba9d510f6
ninopowski <ninoskbd@gmail.com>
2021-09-14 12:53:10 +0200

    Moving and dropping fleet.

--- game_functions.py
+++ game_functions.py
@@ -10,11 +10,13 @@ def get_number_aliens_x(ai_settings, alien_width):
     number_aliens_x = int(available_space_x / (2 * alien_width))
     return number_aliens_x
 
+
 def get_number_rows(ai_settings, ship_height, alien_height):
     """ Determine the number of rows that fit on the screen"""
     available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
     number_rows = int(available_space_y / (2 * alien_height))
     return number_rows
+
 
 def create_alien(ai_settings, screen, aliens, alien_number, row_number):
     """ Create an alien and place it in a row"""
@@ -39,6 +41,18 @@ def create_fleet(ai_settings, screen, ship, aliens):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
 
+def check_fleet_edges(ai_settings, aliens):
+    """ Rrespond appropriarely if any aliens have reached an edge """
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """ Drop the entire fleet and change the fleet direction """
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -102,3 +116,8 @@ def update_buletts(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
+
+def update_aliens(ai_settings, aliens):
+    """ Check for an edge and update the positions of all aliens in the fleet"""
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
--- settings.py
+++ settings.py
@@ -21,3 +21,6 @@ class Settings:
 
         #alien settings
         self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # 1 represents right, -1 left
+        self.fleet_direction = 1
